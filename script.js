52ca3783ccc6fc3a5060b016d289845093561e9e
nwafh9339-beep <nwafh9339@gmail.com>
2026-05-10 08:45:59 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// إعداد الجزيئات (نفس الموقع الأصلي)
+particlesJS("particles-js", {
+    "particles": {
+        "number": { "value": 70, "density": { "enable": true, "value_area": 800 } },
+        "color": { "value": "#ffffff" },
+        "shape": { "type": "circle" },
+        "opacity": { "value": 0.3 },
+        "size": { "value": 2 },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#c5a059",
+            "opacity": 0.2,
+            "width": 1
+        },
+        "move": { "enable": true, "speed": 1.5 }
+    }
+});
+
+// تأثير الكتابة للنص الرئيسي
+new TypeIt("#typewriter", {
+    strings: ["بوابة وزارة العدل", "نظام التحقيقات العسكرية"],
+    speed: 80,
+    loop: true,
+    cursor: false
+}).go();
